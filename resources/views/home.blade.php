03cef8546753b40ff1046cf34223b7dc19dd7839
Vincenzo Ruggiero <vincenzoru@live.it>
2023-02-07 16:43:59 +0100

    Add code to create menu from a phpArray

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,50 @@
+<?php 
+$menuList = ['html', 'css', 'javascript', 'vue', 'php', 'laravel'];
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hompage</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu&display=swap" rel="stylesheet">
+
+    <style>
+        header{
+            margin: 1rem auto;
+        }
+
+        .menu{
+            list-style-type: none;
+            font-size: 1.1rem;
+        }
+
+        .menu .item{
+            display: inline-block;
+            margin-right: 30px;
+        }
+
+        .menu .item a{
+            text-decoration: none;
+        }
+    </style>
+
+    <title>Homepage</title>
 </head>
 <body>
-    <h1>Hello Laravel 👋🏻</h1>
+    <header>
+        <h1>Web Dev Languages 🧑🏻‍💻</h1>
+            <nav>
+                <ul class="menu">
+                    @foreach ($menuList as $item)
+                        <li class="item"><a href={{"/$item"}}>{{$item}}</a></li>
+                    @endforeach    
+                </ul>
+            </nav>
+    </header>
+    
 </body>
 </html>
